bbdf1027a1bd3a7a10e0d7952fda9c6c68835f6a
MarianaShebanova <mariana.shebanova@gmail.com>
2019-11-21 10:55:52 -0800

    Revert "Art"
    This reverts commit 0e845c4d034296e19bfd9aa9d29dd632587fc726.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
+import "./styles.scss";
+import "./App.css";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-import React from "react";
-import { Route } from 'react-router-dom';
-import Header from "./components/Header.js";
-import CharacterList from "./components/CharacterList";
-import Card from "./components/Card";
+import Login from "./components/Login";
+import ProfilePage from './components/ProfilePage.js';
+import PrivateRoute from './components/PrivateRoute';
+import AddForm from "./components/AddForm";
+import Register from "./components/Register";
 
-export default function App() {
+import GalleryList from "./components/Gallery/GalleryList";
+import CardDetails from './components/Gallery/CardDetails';
+
+
+function App() {
+
   return (
-    <main>
-      <Header />
-      <Route exact path="/character/" component={CharacterList}/>
-      <Route exact path="/character/:id" component={Card} />
-    </main>
+    <Router>
+      <div className="App">
+        <Route exact path="/" component={Login} />
+        <Route exact path="/register" component={Register} />
+        <PrivateRoute path="/profile-page">
+          <Route exact path="/profile-page" component={ProfilePage} />
+        </PrivateRoute>
+
+        <Link to="/user-list">Gallery</Link>
+        <Link to="/">Login</Link>
+
+        <Route exact path="/user-list" component={GalleryList} />
+        <Route path="/user-list/:id" component={CardDetails} />
+      </div>
+    </Router>
   );
 }
+
+export default App;
--- src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Header() {
-    return (
-        <header className="ui centered">
-            <h1 className="ui center">Art Portfolios</h1>
-        </header>
-    );
-}
