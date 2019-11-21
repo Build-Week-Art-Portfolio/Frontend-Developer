f8a3043fe55eef88474757aeff6cbab4b700fe8d
kristian-fulkerson <thisistheguy451@gmail.com>
2019-11-21 08:47:50 -0800

    before merge

--- src/components/Gallery/GalleryList.js
+++ src/components/Gallery/GalleryList.js
@@ -3,22 +3,42 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 import GalleryCard from "./GalleryCard";
 
-const GalleryList = props => {
+const GalleryList = () => {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://als-artportfolio.herokuapp.com/users/users")
+      .then(response => {
+        setUsers(response.data);
+        
+        
+      })
+      .catch(error => {
+        console.error("Server Error", error);
+      });
+  }, []);
+
   return (
     <div className="user-container">
-      {props.userPhotos.map(user => {
-        return (
-          <Link to={`/photo-data/${user.id}`}>
-            <GalleryCard
-              key={user.id}
-              name={user.name}
-              avatar={user.avatar}
-              image={user.image}
-              likes={user.likes}
-            />
-          </Link>
-        );
-      })}
+      {users.map(user => (
+        <Link key={user.userid} to={
+          {
+            pathname:`/user-list/${user.userid}`,
+            state: {
+              userID:user.userid,
+              firstName:user.firstname,
+              lastName:user.lastname,
+              profilePic:user.profilepicture
+            }}
+        }>
+          <GalleryCard
+            userID={user.userid}
+            firstName={user.firstname}
+            lastName={user.lastname}
+            profilePic={user.profilepicture}
+          />
+        </Link>
+      ))}
     </div>
   );
 };
