7bbe80bd500fafa502b4e5866c40e45f00e94546
ngaivoronski <ngaivoronski@gmail.com>
2019-11-21 20:17:03 -0500

    card refactor

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ export default function Header() {
     return (
         <header className="ui centered">
             <h1 className="ui center">Art Portfolios</h1>
+            <h2>test</h2>
         </header>
     );
 }
